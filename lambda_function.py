e2e266f6d5d7282d021d16d9409a4ce9c02a2685
yaronkalatian <54883215+yaronkalatian@users.noreply.github.com>
2025-02-20 10:14:29 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -9,14 +9,14 @@ def lambda_handler_ingrerss(event, context):
             cidr_ip = ip_range["cidrIp"]
             if cidr_ip.startswith(("10.", "172.16.", "192.168.")):  # Skip private ranges
                 continue
-            
+
             port = record.get("fromPort", "Unknown")
             group_id = event["detail"]["requestParameters"]["groupId"]
             user = event["detail"]["userIdentity"]["arn"]
 
             message = f"Security Group Rule Change Detected! \n\n"
             message += f"Security Group: {group_id}\nPort: {port}\nCIDR: {cidr_ip}\nUser: {user}"
-            
+
             subject = "Security Group Ingress Alert"
 
     return (message,subject)
@@ -33,19 +33,19 @@ def lambda_handler_s3(event, context):
     return (message,subject)
 
 def lambda_handler_user(event, context):
-    
+
     for record in event.get("detail", {}):
         event_name = event["detail"].get("eventName")
         user_name = event["detail"].get("requestParameters", {}).get("userName")
-        
+
         if event_name == "CreateUser":
             message = f"IAM User Created: {user_name}"
-            subject="IAM create user alert" 
+            subject="IAM create user alert"
 
         elif event_name == "CreateAccessKey":
             access_key_id = event["detail"].get("responseElements", {}).get("accessKey", {}).get("accessKeyId")
             message = f"New Access Key Created for: {user_name}, Access Key ID: {access_key_id}"
-            subject="IAM create access key alert"   
+            subject="IAM create access key alert"
 
 
     return (message,subject)
@@ -55,7 +55,7 @@ def lambda_handler_user(event, context):
 def lambda_handler(event, context):
     sns_client = boto3.client('sns')
     sns_topic_arn = "arn:aws:sns:eu-north-1:183295413583:mySnsHomeTask"  # Replace with your SNS topic ARN
-    
+
     try:
         detail = event.get('detail', {})
         event_name = detail.get('eventName', 'Unknown')
@@ -66,14 +66,22 @@ def lambda_handler(event, context):
             message,subject = lambda_handler_ingrerss(event, context)
         elif  event_name == "PutBucketPolicy":
             message,subject = lambda_handler_s3(event, context)
-            
-       
+        else:
+            message = "Unknown event"
+            subject = "Unknown event"
+
+        event_time = detail.get('eventTime', 'N/A')
+        resource = detail.get('requestParameters', 'Unknown').get("Resource")
+
+        message = f"{message}\n\nEvent Time: {event_time}\nResource: {resource}"
+
+
         sns_client.publish(
             TopicArn=sns_topic_arn,
             Message=message,
             Subject=subject
         )
-        
+
         return {
             'statusCode': 200,
             'body': json.dumps('Alert sent successfully!')
